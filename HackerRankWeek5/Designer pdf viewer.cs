bee66c5267f275af933fdffbf348652d29f97dda
LeeKiraSmithRevature <leekira308@revature.net>
2022-05-11 22:34:36 -0400

    Adding hacker rank week 5

---
+++ HackerRankWeek5/Designer pdf viewer.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<int> h, string word)
+    {
+        char[] alphabet = Enumerable.Range('a', 26).Select(x => (char)x).ToArray();
+        
+        int tempSize = 0;
+        for(int i = 0; i < word.Length; i++){
+            int indexOfChar = Array.IndexOf(alphabet, word[i]);
+            
+            if(h[indexOfChar] >= tempSize)
+                tempSize = h[indexOfChar];
+        }
+        
+        return tempSize * word.Length;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+        string word = Console.ReadLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
